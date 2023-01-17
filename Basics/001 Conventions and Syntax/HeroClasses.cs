66db3c6bcccca19d363a14fd0c8fed92b7f55dd1
Elise Cobabe <122411951+kodielk@users.noreply.github.com>
2023-01-16 20:06:50 -0700

    Added hero Three and a new attack speed variable.

--- Basics/001 Conventions and Syntax/HeroClasses.cs
+++ Basics/001 Conventions and Syntax/HeroClasses.cs
@@ -6,27 +6,43 @@ public class Program
 {
 	public Hero heroOne;
 	public Hero heroTwo;
+	public Hero heroThree;
 	
 	public void Main()
 	{
 		heroOne = new Hero();
 		heroTwo = new Hero();
+		heroThree = new Hero();
 		
 		heroOne.health = 2;
 		heroOne.powerLevel = 5;
+		heroOne.attackSpeed = 8;
 		
 		heroTwo.health = 3;
 		heroTwo.powerLevel = 1;
+		heroTwo.attackSpeed = 10;
+
+		heroThree.health = 7;
+		heroThree.powerLevel = 3;
+		heroThree.attackSpeed = 6;
 		
 		Console.WriteLine(heroOne.health);
 		Console.WriteLine(heroOne.powerLevel);
+		Console.WriteLine(heroOne.attackSpeed);
 		
 		Console.WriteLine(heroTwo.health);
 		Console.WriteLine(heroTwo.powerLevel);
+		Console.WriteLine(heroTwo.attackSpeed);
+
+		Console.WriteLine(heroThree.health);
+		Console.WriteLine(heroThree.powerLevel);
+		Console.WriteLine(heroThree.attackSpeed);
+
 	}
 }
 
 public class Hero {
 	public int health;
 	public int powerLevel;
+	public int attackSpeed;
 }
