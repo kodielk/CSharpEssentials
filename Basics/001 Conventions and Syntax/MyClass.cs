55373bd4b746fb09d992ea2832c0e2f3a7b76d60
Elise Cobabe <122411951+kodielk@users.noreply.github.com>
2023-01-17 19:02:58 -0700

    Made a new class.

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -7,3 +7,33 @@
 //Add another class with any name, add variables 
 //Using the new keyword create several objects
 //Console writeline the Objects variables
+using System;
+
+public class HeroFour{
+    public Hero sova;
+    public Weapon bow;
+
+    public void Main()
+    {
+        sova = new Hero();
+        bow = new Weapon();
+		
+		sova.arrowDmg*= bow.dmgMultiplier;
+        Console.WriteLine(sova.heroName + "\nHealth is " + sova.sovaHealth + "\nCan shoot a shock arrow that does " + sova.baseDmg + " damage." + "\nHe has " + sova.ammoCount + " shock arrows per round.");
+		Console.WriteLine("If the enhanced bow is equipped, the arrows will do " + sova.arrowDmg + " damage.");
+    }
+
+    
+}
+public class Hero{
+	public string heroName = "Sova- Russias Technological Terror";
+    public int sovaHealth = 100;
+	public int baseDmg = 40;
+    public float arrowDmg = 40.0f;
+    public int ammoCount = 2;
+
+}
+
+public class Weapon {
+    public float dmgMultiplier = 2.5f;
+}
