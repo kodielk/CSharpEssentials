9ec578ab4ed6a45107b7b1f75107acd0dd945ffc
Elise Cobabe <122411951+kodielk@users.noreply.github.com>
2023-01-24 18:27:19 -0700

    Added Password Stuff

--- Basics/002FunctionsAndConditionals/Tidbits.cs
+++ Basics/002FunctionsAndConditionals/Tidbits.cs
@@ -4,15 +4,34 @@ public class Program
 {
 	public void Main()
 	{
-		Console.WriteLine("Hello World");
-		DoMath(10);
-        DoMath(20);
-        DoMath(30);
+		Console.WriteLine("Valkommen");
+		DoMath(10, 4);
+		DoMath(20, 7);
+		DoMath(30, 15);
+		Compare(4,3);
+		Compare(3,4);
+		CheckPassword("KEKW");
 	}
 
-	public void DoMath(int value)
+	public void DoMath(int value, int value2)
 	{
-		var number = value + 2;
+		var number = value + value2;
 		Console.WriteLine(number);
+	}
+	
+	public void Compare (int value, int value2){
+		if(value > value2){
+			Console.WriteLine("true, the first is greater");
+		} else {
+			Console.WriteLine("False. The second is greater.");
+		}
+	}
+	
+	public void CheckPassword (string password) {
+		if(password == "KEKW") {
+			Console.WriteLine("Correct Password");
+		} else {
+			Console.WriteLine("Incorrect Password");
+		}
 	}
 }
