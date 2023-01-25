6774e19e53cafa47529bd895ed3cc8f4ace041f5
Elise Cobabe <122411951+kodielk@users.noreply.github.com>
2023-01-24 19:18:46 -0700

    Added the public class "Operations"

--- Basics/002FunctionsAndConditionals/Tidbits.cs
+++ Basics/002FunctionsAndConditionals/Tidbits.cs
@@ -2,35 +2,48 @@ using System;
 
 public class Program
 {
+	public Operations myOperator;
 	public void Main()
 	{
+		myOperator = new Operations();
 		Console.WriteLine("Valkommen");
-		DoMath(10, 4);
-		DoMath(20, 7);
-		DoMath(30, 15);
-		Compare(4,3);
-		Compare(3,4);
-		CheckPassword("KEKW");
+		myOperator.DoMath(10, 4);
+		myOperator.DoMath(20, 7);
+		myOperator.DoMath(30, 15);
+		myOperator.Compare(4, 3);
+		myOperator.Compare(3, 4);
+		myOperator.CheckPassword("KEKW");
 	}
+}
 
+public class Operations
+{
 	public void DoMath(int value, int value2)
 	{
 		var number = value + value2;
 		Console.WriteLine(number);
 	}
-	
-	public void Compare (int value, int value2){
-		if(value > value2){
+
+	public void Compare(int value, int value2)
+	{
+		if (value > value2)
+		{
 			Console.WriteLine("true, the first is greater");
-		} else {
+		}
+		else
+		{
 			Console.WriteLine("False. The second is greater.");
 		}
 	}
-	
-	public void CheckPassword (string password) {
-		if(password == "KEKW") {
+
+	public void CheckPassword(string password)
+	{
+		if (password == "KEKW")
+		{
 			Console.WriteLine("Correct Password");
-		} else {
+		}
+		else
+		{
 			Console.WriteLine("Incorrect Password");
 		}
 	}
